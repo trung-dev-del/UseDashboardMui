c874a2e03fc13095154a6f3bbbb1015e7c0c3676
TienVu <moameicute25702@gmail.com>
2024-12-25 17:36:08 +0700

    thêm tìm kiếm

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,10 +1,53 @@
+"use client"
 import * as React from 'react';
-import { DashboardLayout } from '@toolpad/core/DashboardLayout';
+import { DashboardLayout, ThemeSwitcher } from '@toolpad/core/DashboardLayout';
 import { PageContainer } from '@toolpad/core/PageContainer';
+import { Stack, Tooltip, IconButton, TextField } from '@mui/material';
+import SearchIcon from '@mui/icons-material/Search';
+function ToolbarActionsSearch() {
+  return (
+    <Stack direction="row">
+      <Tooltip title="Search" enterDelay={1000}>
+        <div>
+          <IconButton
+            type="button"
+            aria-label="search"
+            sx={{
+              display: { xs: 'inline', md: 'none' },
+            }}
+          >
+            <SearchIcon />
+          </IconButton>
+        </div>
+      </Tooltip>
+      <TextField
+        label="Search"
+        variant="outlined"
+        size="small"
+        slotProps={{
+          input: {
+            endAdornment: (
+              <IconButton type="button" aria-label="search" size="small">
+                <SearchIcon />
+              </IconButton>
+            ),
+            sx: { pr: 0.5 },
+          },
+        }}
+        sx={{ display: { xs: 'none', md: 'inline-block' }, mr: 1 }}
+      />
+      <ThemeSwitcher />
+    </Stack>
+  );
+}
 
 export default function Layout(props: { children: React.ReactNode }) {
   return (
-    <DashboardLayout>
+    <DashboardLayout
+    slots={{
+      toolbarActions: () => <ToolbarActionsSearch />, // Sử dụng callback để render component
+    }}
+    >
       <PageContainer>{props.children}</PageContainer>
     </DashboardLayout>
   );
